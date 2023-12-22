115097b60d1f6b2802ebfa66762454f4c9e432e4
mahegz <hegzmahmoud@gmail.com>
2023-12-22 12:38:24 +0100

    bug fixed

--- test_attacks/attack.py
+++ test_attacks/attack.py
@@ -6,81 +6,132 @@ from argparse import ArgumentParser
 from sklearn.metrics import accuracy_score
 from torch.utils.data import DataLoader
 import sys
-sys.path.append('/content/PGM-project')
 
-from configs import * 
+sys.path.append("/home/cmap/Desktop/MVA/PGM")
+# print(sys.path)
+from configs import *
 from test_attacks.load.load_classifier import load_classifier
 
 # Assuming you have already imported torch and torch.nn.functional as F
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 
 def test_attacks(data_name, model_name, attack_method, eps, batch_size, targeted=False):
-    if data_name == 'mnist':
+    print("Training the model")
+    if data_name == "mnist":
         from utils import data_mnist
-        X_train, Y_train, X_test, Y_test = data_mnist(train_start=0, train_end=25000, test_start=0, test_end=64)
+
+        X_train, Y_train, X_test, Y_test = data_mnist(
+            train_start=0, train_end=25000, test_start=0, test_end=64
+        )
         X_train = X_train / 255.0
         X_test = X_test / 255.0
+
     source_samples, img_rows, img_cols = len(X_test), X_test.shape[0], X_test.shape[1]
     nb_classes = 10
-    
+    # Training the model on the train set and evaluating on test set with and without adversarial examples
+    data_loader_train = DataLoader(
+        list(
+            zip(
+                X_train.clone().detach().reshape(-1, 1, 28, 28).to(device),
+                Y_train.clone().detach().to(device),
+            )
+        ),
+        batch_size=batch_size,
+        shuffle=True,
+    )
     model = load_classifier(model_name, data_name)
     model = model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    model.train_model(model_name, data_loader_train, optimizer, num_epochs=1)
+
+    # Evaluate the model on clean examples
+    p_y_pred = model.predict(
+        model_name,
+        torch.tensor(X_test[:source_samples].reshape(-1, 1, 28, 28)).to(device),
+    )
+    _, y_preds = torch.max(p_y_pred, 1)
+    accuracy = accuracy_score(
+        Y_test[:source_samples], y_preds.cpu().numpy()
+    )  # Move tensor to CPU before converting to numpy
+    print("Test accuracy on clean examples: {:.4f}".format(accuracy))
+    raise NotImplementedError("part of code not done")
+
+
+    print("Testing Attacks on Model: ", model_name)
     if targeted:
-        adv_inputs = np.array([instance for instance in X_test[:source_samples]], dtype=np.float32)
+        adv_inputs = np.array(
+            [instance for instance in X_test[:source_samples]], dtype=np.float32
+        )
         one_hot = np.zeros((nb_classes, nb_classes), dtype=np.float32)
         one_hot[np.arange(nb_classes), np.arange(nb_classes)] = 1
-        adv_inputs = adv_inputs.reshape((source_samples * nb_classes, img_rows, img_cols, 1))
-        adv_ys = np.array([one_hot] * source_samples, dtype=np.float32).reshape((source_samples * nb_classes, nb_classes))
+        adv_inputs = adv_inputs.reshape(
+            (source_samples * nb_classes, img_rows, img_cols, 1)
+        )
+        adv_ys = np.array([one_hot] * source_samples, dtype=np.float32).reshape(
+            (source_samples * nb_classes, nb_classes)
+        )
     else:
         adv_inputs = X_test[:source_samples]
         adv_ys = Y_test[:source_samples]
-    
+
     adv_inputs = adv_inputs.clone().detach().view(-1, 1, 28, 28).to(device)
     adv_ys = adv_ys.clone().detach().to(device)
 
     attack, attack_params = load_attack(attack_method)
-    
+
     # Perform the attack
     adv_examples = []
-    #one hot encoding of y
+    # one hot encoding of y
     y = torch.zeros((source_samples, nb_classes)).to(device)
     y[np.arange(source_samples), adv_ys] = 1
     for i in range(0, source_samples, batch_size):
-        adv_batch = attack(model, adv_inputs[i:min(source_samples,i+batch_size)], y[i:min(source_samples,i+batch_size)], **attack_params)
-        adv_examples.extend(adv_batch.cpu().detach().numpy())  # Move tensor to CPU before converting to numpy
+        adv_batch = attack(
+            model,
+            adv_inputs[i : min(source_samples, i + batch_size)],
+            y[i : min(source_samples, i + batch_size)],
+            **attack_params
+        )
+        adv_examples.extend(
+            adv_batch.cpu().detach().numpy()
+        )  # Move tensor to CPU before converting to numpy
     adv_examples = np.array(adv_examples, dtype=np.float32)
-    print('-'*30)
-    # Training the model on the train set and evaluating on test set with and without adversarial examples
-    data_loader_train = DataLoader(list(zip(X_train.clone().detach().reshape(-1, 1, 28, 28).to(device), Y_train.clone().detach().to(device))), batch_size=batch_size, shuffle=True)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-    model.train_model(model_name, data_loader_train, optimizer, num_epochs=25)
+    print("-" * 30)
 
-    
-    # Evaluate the model on clean examples
-    p_y_pred = model.predict(model_name, torch.tensor(X_test[:source_samples].reshape(-1, 1, 28, 28)).to(device))
-    _, y_preds = torch.max(p_y_pred, 1)
-    accuracy = accuracy_score(Y_test[:source_samples], y_preds.cpu().numpy())  # Move tensor to CPU before converting to numpy
-    print('Test accuracy on clean examples: {:.4f}'.format(accuracy))
-    
+
+
     # Evaluate the model on adversarial examples
-    p_y_pred = model.predict(model_name, torch.tensor(adv_examples.reshape(-1, 1, 28, 28)).to(device))
+    p_y_pred = model.predict(
+        model_name, torch.tensor(adv_examples.reshape(-1, 1, 28, 28)).to(device)
+    )
     _, y_preds = torch.max(p_y_pred, 1)
-    accuracy = accuracy_score(Y_test[:source_samples], y_preds.cpu().numpy())  # Move tensor to CPU before converting to numpy
-    print('Test accuracy on adversarial examples: {:.4f}'.format(accuracy))
+    accuracy = accuracy_score(
+        Y_test[:source_samples], y_preds.cpu().numpy()
+    )  # Move tensor to CPU before converting to numpy
+    print("Test accuracy on adversarial examples: {:.4f}".format(accuracy))
 
     return adv_examples
 
+
 def main():
-    parser = ArgumentParser(description='Run adversarial attacks.')
-    parser.add_argument('--data_name', type=str, default='mnist')
-    parser.add_argument('--model_name', type=str, default='GBZ')
-    parser.add_argument('--attack_method', type=str, default='fgsm')
-    parser.add_argument('--eps', type=float, default=0.1)
-    parser.add_argument('--batch_size', type=int, default=100)
-    parser.add_argument('--targeted', action='store_true', default=False)
+    parser = ArgumentParser(description="Run adversarial attacks.")
+    parser.add_argument("--data_name", type=str, default="mnist")
+    parser.add_argument("--model_name", type=str, default="GBZ")
+    parser.add_argument("--attack_method", type=str, default="fgsm")
+    parser.add_argument("--eps", type=float, default=0.1)
+    parser.add_argument("--batch_size", type=int, default=100)
+    parser.add_argument("--targeted", action="store_true", default=False)
     args = parser.parse_args()
 
-    test_attacks(args.data_name, args.model_name, args.attack_method, args.eps, args.batch_size, args.targeted)
+    test_attacks(
+        args.data_name,
+        args.model_name,
+        args.attack_method,
+        args.eps,
+        args.batch_size,
+        args.targeted,
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- test_attacks/load/load_bayes_classifier.py
+++ test_attacks/load/load_bayes_classifier.py
@@ -1,62 +1,143 @@
-
-
 import numpy as np
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
 import sys
-sys.path.append('/content/PGM-project')
-sys.path.append('/content/PGM-project')
-sys.path.append('/content/PGM-project')
-sys.path.append('../')
-sys.path.append('../test_attacks')
-sys.path.append('/home/boubacar/Documents/PGM project/PGM-project/test_attacks')
-sys.path.append('./PGM-project/test_attacks')
-sys.path.append('/home/boubacar/Documents/PGM project/PGM-project/models')
+
+sys.path.append("/content/PGM-project")
+sys.path.append("/content/PGM-project")
+sys.path.append("/content/PGM-project")
+sys.path.append("../")
+sys.path.append("../test_attacks")
+sys.path.append("/home/boubacar/Documents/PGM project/PGM-project/test_attacks")
+sys.path.append("./PGM-project/test_attacks")
+sys.path.append("/home/boubacar/Documents/PGM project/PGM-project/models")
 
 
 import matplotlib.pyplot as plt
+
 # Assuming you have already imported torch and torch.nn.functional as F
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def log_gaussian_prob(x, mu, log_sig):
+    logprob = (
+        -(0.5 * torch.log(2 * torch.pi * torch.ones(1).to(device)) + log_sig)
+        - 0.5 * ((x - mu) / torch.exp(log_sig)) ** 2
+    )
+    return torch.sum(logprob, dim=1)
+
+
+
+
+
+def log_bernoulli_prob(x, p=0.5):
+    logprob = x * torch.log(torch.clamp(p, 1e-9, 1.0)) + (1 - x) * torch.log(
+        torch.clamp(1.0 - p, 1e-9, 1.0)
+    )
+    ind = list(range(1, len(x.shape)))
+    return torch.sum(logprob, ind)
+
+
+def encoding(enc, x, y, K, use_mean=False):
+    mu_qz, log_sig_qz = enc(x, y)
+    if use_mean and K>1:
+        raise AssertionError("cannot use mean if k>1")
+    if use_mean:
+        return mu_qz.unsqueeze(0), log_gaussian_prob(
+            mu_qz.unsqueeze(0), mu_qz.unsqueeze(0), log_sig_qz.unsqueeze(0)
+        )
+    ph = torch.zeros([K] + list(mu_qz.shape))
+    norm_sample = torch.normal(ph).to(device)
+    samples = mu_qz + torch.unsqueeze(log_sig_qz.exp(), dim=0) * norm_sample
+    logq = log_gaussian_prob(samples, mu_qz.unsqueeze(0), log_sig_qz.unsqueeze(0))
+    return samples, logq
+
+
+def lowerbound_F(x, y, enc, dec, K=1, IS=False, beta=1.0, use_mean=False):
+    # print("Inputs", x.shape, y.shape)
+    z, logq = encoding(enc, x, y, K, use_mean)
+    # print("Encoding", z.shape, logq.shape)
+    log_prior_z = log_gaussian_prob(z, torch.zeros_like(z), torch.zeros_like(z))
+    pxz, pyz = dec(torch.flatten(z, end_dim=1))
+    pxz = torch.stack(torch.tensor_split(pxz, K, dim=0), dim=0)
+    pyz = torch.stack(torch.tensor_split(pyz, K, dim=0), dim=0)
+    # print("z shape", z.shape)
+    ind = list(range(2, len(x.shape) + 1))
+    # print("pxz shape: ", pxz.shape)
+    # print("x shape: ", x.unsqueeze(0).shape)
+    logp = -torch.sum((x.unsqueeze(0) - pxz) ** 2, dim=ind)
+    logit_y = F.softmax(pyz, dim=2)
+
+    y_rep = torch.stack([y for i in range(K)], dim=0)
+    log_pyz = -F.cross_entropy(
+        logit_y.flatten(end_dim=1), y_rep.flatten(end_dim=1), reduction="none"
+    ).reshape(y_rep.shape[:-1])
+    bound = logp * beta + log_pyz + (log_prior_z - logq)
+    if IS and K > 1 and use_mean == False:
+        bound = torch.logsumexp(bound, dim=0) - np.log(float(K))
+    return bound.squeeze()
+
+
+def bayes_classifier(
+    lowerbound, x, enc, dec, dimY, K=1, beta=1.0, use_mean=False
+):
+    N = x.shape[0]
+    logpxy = []
+    for i in range(dimY):
+        y = torch.zeros([N, dimY]).to(device)
+        y[:, i] = 1
+        # print(x.shape, y.shape)
+        bound = lowerbound(x, y, enc, dec, K, IS=True, beta=beta, use_mean=use_mean)
+        logpxy.append(torch.unsqueeze(bound, 1))
+    logpxy = torch.concat(logpxy, 1)
+    pyx = F.softmax(logpxy, dim=1)
+    return pyx
+
 
 class BayesModel(nn.Module):
     def __init__(self, model_name, data_name, hidden_channels, dimZ=64):
         super(BayesModel, self).__init__()
-        if data_name == 'mnist':
+        if data_name == "mnist":
             self.num_channels = 1
             self.image_size = 28
             self.num_labels = 10
-        if data_name == 'cifar10':
-            self.num_channels = 3
-            self.image_size = 32
-            self.num_labels = 10
+
         self.hidden_channels = hidden_channels
         self.use_mean = True
         self.training = True
-        
-        if model_name == 'GBZ':
+
+        if model_name == "GBZ":
             from models.gbz import Encoder, Decoder
-        elif model_name == 'DBX':
+        elif model_name == "DBX":
             from models.dbx import Encoder, Decoder
         else:
-            raise ValueError('model type not recognised')
-        
-        self.encoder = Encoder(hidden_channels=self.hidden_channels, latent_dim=dimZ, num_labels=self.num_labels)
-        self.decoder = Decoder(hidden_channels=self.hidden_channels, latent_dim=dimZ, num_labels=self.num_labels)
+            raise ValueError("model type not recognised")
+
+        self.encoder = Encoder(
+            hidden_channels=self.hidden_channels,
+            latent_dim=dimZ,
+            num_labels=self.num_labels,
+        )
+        self.decoder = Decoder(
+            hidden_channels=self.hidden_channels,
+            latent_dim=dimZ,
+            num_labels=self.num_labels,
+        )
 
         # MLP for p(y|z)
         self.fc_py_z = nn.Linear(dimZ, self.num_labels)
-        
+
     def log_softmax_prob(self, z, y):
         """
         Compute the log probability log p(y|z).
         """
         logit_y = self.fc_py_z(z)
-        log_p_y_z = -F.cross_entropy(logit_y, torch.argmax(y, dim=1), reduction='none')
+        log_p_y_z = -F.cross_entropy(logit_y, torch.argmax(y, dim=1), reduction="none")
         return log_p_y_z
-    
+
     def forward(self, x, y):
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         x = x.to(device)
         y = y.to(device)
         latent_mu, latent_logvar = self.encoder(x, y)
@@ -67,7 +148,6 @@ class BayesModel(nn.Module):
         x_recon, y_pred = self.decoder(latent)
         return x_recon, y_pred, latent_mu, latent_logvar
 
-    
     def latent_sample(self, mu, logvar):
         if self.training:
             std = torch.exp(0.5 * logvar)
@@ -75,74 +155,20 @@ class BayesModel(nn.Module):
             return mu + eps * std
         else:
             return mu
-    
-    def log_gaussian_prob(self, x, mu, log_sig):
-        logprob = -(0.5 * torch.log(2 * torch.pi * torch.ones(1).to(device)) + log_sig) \
-                    - 0.5 * ((x - mu) / torch.exp(log_sig)) ** 2
-        return torch.sum(logprob, dim=1)
 
-    
-    def log_bernoulli_prob(self, x, p=0.5):
-        logprob = x * torch.log(torch.clamp(p, 1e-9, 1.0)) \
-                + (1 - x) * torch.log(torch.clamp(1.0 - p, 1e-9, 1.0))
-        ind = list(range(1, len(x.shape)))
-        return torch.sum(logprob, ind)
-    
-    def lowerbound_F(self, x, y, enc, dec, K=1, IS=False,  beta=1.0, use_mean=False):
-
-        z, logq = self.encoding(enc,x, y, K, use_mean)
-        log_prior_z = self.log_gaussian_prob(z, torch.zeros_like(z), torch.zeros_like(z))
-        pxz, pyz = dec(torch.flatten(z,end_dim=1))
-        pxz = torch.stack(torch.tensor_split(pxz,K,dim=0),dim=0)
-        pyz = torch.stack(torch.tensor_split(pyz,K,dim=0),dim=0)
-        ind = list(range(2, len(x.shape)+1))
-        print("pxz shape: ", pxz.shape)
-        print("x shape: ", x.unsqueeze(0).shape)
-        logp = -torch.sum((x.unsqueeze(0) - pxz)**2, dim=ind)
-        logit_y = F.softmax(pyz,dim=2)
-
-        y_rep = torch.stack([y  for i in range(K)],dim=0)
-        log_pyz = -F.cross_entropy(logit_y.flatten(end_dim=1), y_rep.flatten(end_dim=1),
-                                reduction='none').reshape(y_rep.shape[:-1])
-        bound = logp * beta + log_pyz + (log_prior_z - logq)
-        if IS and K > 1 and use_mean==False:
-            bound = torch.logsumexp(bound,dim=0) - np.log(float(K))
-        return bound.squeeze()
-    
-    def encoding(self, enc, x, y, K, use_mean=False):
-        mu_qz, log_sig_qz = enc(x, y)
-        if use_mean:
-            return mu_qz.unsqueeze(0), self.log_gaussian_prob(mu_qz.unsqueeze(0), mu_qz.unsqueeze(0), log_sig_qz.unsqueeze(0))
-        ph = torch.zeros([K]+list(mu_qz.shape))
-        norm_sample = torch.normal(ph).to(device)
-        samples = mu_qz+torch.unsqueeze(log_sig_qz.exp(), dim=0)*norm_sample
-        logq = self.log_gaussian_prob(samples, mu_qz.unsqueeze(0), log_sig_qz.unsqueeze(0))
-        return samples, logq
-    
-    def bayes_classifier(self, lowerbound, x, enc, dec, dimY,  K = 1, beta=1.0, use_mean=False):
-        N = x.shape[0]
-        logpxy = []
-        for i in range(dimY):
-            y = torch.zeros([N, dimY]).to(device)
-            y[:, i] = 1
-            bound = lowerbound(x, y,enc, dec, K,
-                            IS=True, beta=beta, use_mean=use_mean)
-            logpxy.append(torch.unsqueeze(bound, 1))
-        logpxy = torch.concat(logpxy, 1)
-        pyx = F.softmax(logpxy,dim=1)
-        return pyx
-
-
-    def train_model(self, model_name, data_loader, optimizer, num_epochs=30, variational_beta=1.0):
-        if model_name == 'GBZ':
+    def train_model(
+        self, model_name, data_loader, optimizer, num_epochs=30, variational_beta=1.0
+    ):
+        if model_name == "GBZ":
             self.train_gbz(data_loader, optimizer, num_epochs, variational_beta)
-        elif model_name == 'DBX':
+        elif model_name == "DBX":
             self.train_dbx(data_loader, optimizer, num_epochs, variational_beta)
         else:
-            raise ValueError('model type not recognised')    
-        
-    def train_gbz(self, data_loader, optimizer, num_epochs=30, variational_beta=1.0):
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+            raise ValueError("model type not recognised")
+
+    def train_gbz(self, data_loader, optimizer, num_epochs=1, variational_beta=1.0):
+        print("Traning GBZ")
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.train()  # Set the model to training mode
         for epoch in range(num_epochs):
             for x, y in data_loader:
@@ -156,43 +182,54 @@ class BayesModel(nn.Module):
 
                 # Forward pass
                 x_recon, y_pred, latent_mu, latent_logvar = self.forward(x, y_one_hot)
-               
-                recon_loss = F.mse_loss(x_recon, x, reduction='sum')
+
+                recon_loss = F.mse_loss(x_recon, x, reduction="sum")
                 # print("trues: ", y[:20])
                 # print("preds: ", torch.max(y_pred, 1)[1][:20])
-                label_loss = F.cross_entropy(y_pred, y, reduction='sum')  # Use y_pred directly
-                kldivergence = -0.5 * torch.sum(1 + latent_logvar - latent_mu.pow(2) - latent_logvar.exp())
+                label_loss = F.cross_entropy(
+                    y_pred, y, reduction="sum"
+                )  # Use y_pred directly
+                kldivergence = -0.5 * torch.sum(
+                    1 + latent_logvar - latent_mu.pow(2) - latent_logvar.exp()
+                )
                 loss = recon_loss + label_loss + variational_beta * kldivergence
 
                 loss.backward()  # Compute the gradients
                 optimizer.step()  # Update the model parameters
 
             print(f"Epoch {epoch + 1}/{num_epochs} Loss: {loss.item()}")
-            
-    def train_dbx(self, data_loader, optimizer, num_epochs=30, variational_beta=1.0, K=100):
+
+    def train_dbx(
+        self, data_loader, optimizer, num_epochs=30, variational_beta=1.0, K=100
+    ):
         pass
-    
+
     def predict(self, model_name, x, K=8, batch_size=10):
-      """
-      Predict the class probabilities for the input x using importance sampling.
-      """
-      if model_name == 'GBZ':
-          return self.predict_GBZ(x, K)
-      elif model_name == 'DBX':
-          return self.predict_DBX(x, K)
-      else:
-          raise ValueError('model type not recognised')
+        """
+        Predict the class probabilities for the input x using importance sampling.
+        """
+        # print("model_name", model_name)
+        if model_name == "GBZ":
+            return self.predict_GBZ(x, K)
+        elif model_name == "DBX":
+            return self.predict_DBX(x, K)
+        else:
+            raise ValueError("model type not recognised")
 
     def predict_GBZ(self, x, K=10):
         """
         Predict the class probabilities for the input x using importance sampling.
         """
-        return self.bayes_classifier(self.lowerbound_F, x, self.encoder, self.decoder, self.num_labels, K=K, beta=1.0, use_mean=True)
-      
+        return bayes_classifier(
+            lowerbound_F,
+            x,
+            self.encoder,
+            self.decoder,
+            self.num_labels,
+            K=K,
+            beta=1.0,
+            use_mean=False,
+        )
+
     def predict_DBX(self, x, K=10, batch_size=10):
         pass
-
-
-
-
-
